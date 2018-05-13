3de0c55e2ab68d0f667ba2418eec732e8e7f2f40
Patrick Outler <outler.patrick@gmail.com>
2018-05-13 11:28:15 -0400

    First Steps
    Use a feature branch next time

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/index.css";
+import App from "./components/App";
+import registerServiceWorker from "./registerServiceWorker";
+import { ApolloProvider } from "react-apollo";
+import { ApolloClient } from "apollo-client";
+import { httpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const httpLink = new HttpLink({ uri: "http://localhost:4000" });
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
